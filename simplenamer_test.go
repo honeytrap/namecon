2de68cb0db4d2044d2d112d8f1903929160ef312
Alexander Ewetumo <trinoxf@gmail.com>
2017-03-29 12:39:21 +0100

    Add Namer with configurable lengths

--- simplenamer_test.go
+++ simplenamer_test.go
@@ -1,6 +1,7 @@
 package namecon_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/honeytrap/namecon"
@@ -18,4 +19,22 @@ func TestSimpleNamer(t *testing.T) {
 		t.Fatalf("Should have successfully generated new unique generation names")
 	}
 	t.Logf("Should have successfully generated new unique generation names")
+}
+
+// TestLimitedNamer validates the use of the provided namer to match the
+// giving template rule set provided.
+func TestLimitedNamer(t *testing.T) {
+	namer := namecon.GenerateNamer(namecon.NewLimitNamer(50, 10), "API-%s-%s")
+
+	newName := namer("TrappaHouseOfSundaySchool")
+	if len(newName) > 50 {
+		t.Fatalf("Should have successfully new name within 120 characters")
+	}
+	t.Logf("Should have successfully new name within 120 characters")
+
+	parts := strings.Split(newName, "-")
+	if len(parts[1]) > 10 {
+		t.Fatalf("Should have successfully generated base name piece under 20 characters")
+	}
+	t.Logf("Should have successfully generated base name piece under 20 characters")
 }
